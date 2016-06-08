01c5179fded6b98114d0266da0442e0b5c9e3c69
Iglin <iglincontact@gmail.com>
2016-06-09 01:15:03 +0400

    models editor js controller alerts

--- src/main/webapp/resources/js/controllers/modelsEditorController.js
+++ src/main/webapp/resources/js/controllers/modelsEditorController.js
@@ -17,32 +17,88 @@ modelsEditor.controller('modelsEditorController', function($scope, $http, $route
     } else {
         $scope.isUpdate = false;
     }
+    
+    function validateInputs() {
+        if ($scope.model == null) {
+            showSimpleAlert(false, "Необходимо ввести данные о модели.");
+            return false;
+        }
+        if (isEmpty($scope.model.name)) {
+            showSimpleAlert(false, "Необходимо ввести название модели.");
+            return false;
+        }
+        if (isEmpty($scope.model.manufacturer)) {
+            showSimpleAlert(false, "Необходимо ввести производителя.");
+            return false;
+        }
+        if (!isValidFloat($scope.model.price, 0, null)) {
+            showSimpleAlert(false, "Некорректно указана цена.");
+            return false;
+        }
+        if (!isValidInt($scope.model.calibrationPeriod, 1, null)) {
+            showSimpleAlert(false, "Некорректно указан межпроверочный интервал.");
+            return false;
+        }
+        if (!isValidInt($scope.model.ratesCount, 1, null)) {
+            showSimpleAlert(false, "Некорректно указано кол-во тарифов.");
+            return false;
+        }
+        if (isEmpty($scope.model.meterInterface)) {
+            showSimpleAlert(false, "Необходимо ввести интерфейс.");
+            return false;
+        }
+        if (!isValidInt($scope.model.atomicity, 1, null)) {
+            showSimpleAlert(false, "Некорректно указана значность.");
+            return false;
+        }
+        if (isEmpty($scope.model.accuracy)) {
+            showSimpleAlert(false, "Необходимо ввести класс точности.");
+            return false;
+        }
+        if (!isValidInt($scope.model.nominalCurrent, 0, null)) {
+            showSimpleAlert(false, "Некорректно указан номинальный ток.");
+            return false;
+        }
+        if (!isValidInt($scope.model.maximalCurrent, 0, null)) {
+            showSimpleAlert(false, "Некорректно указан максимальный ток.");
+            return false;
+        }
+        if (!isValidInt($scope.model.nominalVoltage, 0, null)) {
+            showSimpleAlert(false, "Некорректно указано номинальное напряжение.");
+            return false;
+        }
+        return true;
+    }
 
     $scope.add = function () {
-        $http({
-            url: '/models/editor/',
-            method: 'POST',
-            params: {
-                model: $scope.model
-            }
-        }).then(function (response) {
-            alert(response.data);
-        }, function (response) {
-            alert(JSON.stringify(response));
-        });
+        if (validateInputs()) {
+            $http({
+                url: '/models/editor/',
+                method: 'POST',
+                params: {
+                    model: $scope.model
+                }
+            }).then(function (response) {
+                showAlert(response);
+            }, function (response) {
+                alert(JSON.stringify(response));
+            });
+        }
     };
 
     $scope.update = function () {
-        $http({
-            url: '/models/editor/',
-            method: 'PUT',
-            params: {
-                model: $scope.model
-            }
-        }).then(function (response) {
-            alert(response.data);
-        }, function (response) {
-            alert(JSON.stringify(response));
-        });
-    };
+        if (validateInputs()) {
+            $http({
+                url: '/models/editor/',
+                method: 'PUT',
+                params: {
+                    model: $scope.model
+                }
+            }).then(function (response) {
+                showAlert(response);
+            }, function (response) {
+                alert(JSON.stringify(response));
+            });
+        }
+    }
 });

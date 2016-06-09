a67968a65b2dfcace137ede8ec21ece7534aeb30
Iglin <iglincontact@gmail.com>
2016-06-09 21:22:08 +0400

    user profile input control and warning

--- src/main/webapp/resources/js/controllers/userProfileController.js
+++ src/main/webapp/resources/js/controllers/userProfileController.js
@@ -9,24 +9,54 @@ userProfile.controller('userProfileController', function($scope, $http, $routePa
     }).then(function(response){
         $scope.user = response.data;
     }, function(response){
-        alert(JSON.stringify(response));
+        showAlert(response);
     });
 
+    function validateInput() {
+        if ($scope.user == null) {
+            showSimpleAlert(false, "Необходимо ввести логин и пароль.");
+            return false;
+        }
+        if (isEmpty($scope.user.login)) {
+            showSimpleAlert(false, "Необходимо ввести логин.");
+            return false;
+        } else if ($scope.user.login.length < 4 || $scope.user.login.length > 16) {
+            showSimpleAlert(false, "Допустимая длина имени пользователя от 4 до 16 символов.");
+            return false;
+        } else if ($scope.user.login.indexOf(' ') != -1) {
+            showSimpleAlert(false, "Имя пользователя не может содержать пробелы.");
+            return false;
+        }
+        if (isEmpty($scope.user.password)) {
+            showSimpleAlert(false, "Необходимо ввести пароль.");
+            return false;
+        } else if ($scope.user.password.length < 4 || $scope.user.password.length > 16) {
+            showSimpleAlert(false, "Допустимая длина пароля от 4 до 16 символов.");
+            return false;
+        } else if ($scope.user.password.indexOf(' ') != -1) {
+            showSimpleAlert(false, "Пароль не может содержать пробелы.");
+            return false;
+        }
+        return true;
+    }
+
     $scope.update = function () {
-        if ($scope.user.password == $scope.password2) {
-            $http({
-                url: '/user/',
-                method: 'PUT',
-                params: {
-                    user: $scope.user
-                }
-            }).then(function (response) {
-                //   alert(response.data);
-            }, function (response) {
-                alert(JSON.stringify(response));
-            });
-        } else {
-            alert('Пароли не совпадают!');
+        if (validateInput()) {
+            if ($scope.user.password == $scope.password2) {
+                $http({
+                    url: '/user/',
+                    method: 'PUT',
+                    params: {
+                        user: $scope.user
+                    }
+                }).then(function (response) {
+                    window.location.href = "/logout";
+                }, function (response) {
+                    showAlert(response);
+                });
+            } else {
+                showSimpleAlert(false, 'Пароли не совпадают!');
+            }
         }
     };
 });
